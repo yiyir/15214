9ade8b55749cd3f71f26529cc5cf80e7baa9c9b5
mac of yiyi <apple@applematoMacBook-Air.local>
2017-09-20 17:42:43 -0400

    recitation04 completed

--- recitation/04/src/main/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntList.java
+++ recitation/04/src/main/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntList.java
@@ -1,30 +1,72 @@
 package edu.cmu.cs.cs214.rec04;
 
 /**
- * DelegationSortedIntList -- a variant of a SortedIntList that keeps 
- * count of the number of attempted element insertions (not to be confused
- * with the current size, which goes down when an element is removed) 
- * and exports an accessor (totalAdded) for this count.
+ * DelegationSortedIntList -- a variant of a SortedIntList that keeps count of
+ * the number of attempted element insertions (not to be confused with the
+ * current size, which goes down when an element is removed) and exports an
+ * accessor (totalAdded) for this count.
  * 
  * @author Nora Shoemaker
  *
  */
-									  
-// HINT: Take a look at the UML diagram to see what DelegationSortedIntList 
-//       should implement.
-public class DelegationSortedIntList { 
-	// the number of attempted element insertions
-	private int totalAdded;
-	
-	/**
-	 * Gets the total number of attempted int insertions to the list since it.
-	 * was created.
-	 * 
-	 * @return total number of integers added to the list.
-	 */
-	public int getTotalAdded()
-	{
-		return totalAdded;
-	}
+
+// HINT: Take a look at the UML diagram to see what DelegationSortedIntList
+// should implement.
+public class DelegationSortedIntList implements IntegerList {
+    // the number of attempted element insertions
+    private int totalAdded;
+    private SortedIntList list;
+
+    public DelegationSortedIntList() {
+        this.list = new SortedIntList();
+    }
+
+    public int getTotalAdded() {
+        return this.totalAdded;
+    }
+
+    @Override
+    public boolean add(int num) {
+        boolean success = this.list.add(num);
+        if (success) {
+            totalAdded++;
+
+        }
+
+        return success;
+    }
+
+    @Override
+    public boolean addAll(IntegerList list) {
+        int oldSize = this.list.size();
+        this.list.addAll(list);
+        int newSize = this.list.size();
+        totalAdded += (newSize - oldSize);
+        return false;
+    }
+
+    @Override
+    public int get(int index) {
+        // TODO Auto-generated method stub
+        return this.list.get(index);
+    }
+
+    @Override
+    public boolean remove(int num) {
+        // TODO Auto-generated method stub
+        return this.list.remove(num);
+    }
+
+    @Override
+    public boolean removeAll(IntegerList list) {
+        // TODO Auto-generated method stub
+        return this.list.removeAll(list);
+    }
+
+    @Override
+    public int size() {
+        // TODO Auto-generated method stub
+        return this.list.size();
+    }
 
 }
--- recitation/04/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
+++ recitation/04/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
@@ -1,27 +1,46 @@
 package edu.cmu.cs.cs214.rec04;
 
 /**
- * InheritanceSortedIntList -- a variant of a SortedIntList that keeps 
- * count of the number of attempted element insertions (not to be confused
- * with the current size, which goes down when an element is removed) 
- * and exports an accessor (totalAdded) for this count.
+ * InheritanceSortedIntList -- a variant of a SortedIntList that keeps count of
+ * the number of attempted element insertions (not to be confused with the
+ * current size, which goes down when an element is removed) and exports an
+ * accessor (totalAdded) for this count.
  * 
  * @author Nora Shoemaker
  *
  */
 public class InheritanceSortedIntList extends SortedIntList {
-	// the number of attempted element insertions
-	private int totalAdded;
-	
-	/**
-	 * Gets the total number of attempted int insertions to the list since it
-	 * was created.
-	 * 
-	 * @return total number of integers added to the list.
-	 */
-	public int getTotalAdded()
-	{
-		return this.totalAdded;
-	}
+    // the number of attempted element insertions
+    private int totalAdded;
+
+    /**
+     * Gets the total number of attempted int insertions to the list since it
+     * was created.
+     * 
+     * @return total number of integers added to the list.
+     */
+    public int getTotalAdded() {
+        return this.totalAdded;
+    }
+
+    @Override
+    public boolean add(int num) {
+        boolean success = super.add(num);
+        if (success) {
+            totalAdded++;
+        }
+        return success;
+    }
+
+    @Override
+    public boolean addAll(IntegerList list) {
+        boolean success = false;
+
+        for (int i = 0; i < list.size(); i++) {
+            success |= this.add(list.get(i));
+        }
+
+        return success;
+    }
 
 }
