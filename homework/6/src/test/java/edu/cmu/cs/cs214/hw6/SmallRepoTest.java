9bfa46939a8e1e57eaf5f3d8b550d52b462d6a00
Yiyi Ren <yiyiren@Yiyis-MacBook-Pro.local>
2017-12-04 19:39:41 -0500

    checkpoint commit

--- homework/6/src/test/java/edu/cmu/cs/cs214/hw6/SmallRepoTest.java
+++ homework/6/src/test/java/edu/cmu/cs/cs214/hw6/SmallRepoTest.java
@@ -8,19 +8,21 @@ import java.util.List;
 
 import static org.junit.Assert.*;
 
-public class SequentialGitAnalysisTest {
+public class SmallRepoTest {
     private static final String PATH_NAME = "/Users/yiyiren/churn-rate-example/.git";
-    private SequentialGitAnalysis analysis;
+    private SequentialGitAnalysis smallRepoAnalysis;
+
 
     @Before
     public void setUp() throws Exception {
-        analysis = new SequentialGitAnalysis(new File(PATH_NAME));
-        analysis.getDevelopmentHistory();
+        smallRepoAnalysis = new SequentialGitAnalysis(new File(PATH_NAME));
+        smallRepoAnalysis.getDevelopmentHistory();
+
     }
 
     @Test
     public void getChildCommits() throws Exception {
-        List<String> childCommits = analysis.getChildCommits();
+        List<String> childCommits = smallRepoAnalysis.getChildCommits();
         assertTrue(childCommits.size() == 2);
         assertTrue(childCommits.get(0).equals("b0eebd2c20ee60221cb5411108237d76706abf72"));
         assertTrue(childCommits.get(1).equals("34db5b16e46ec74f9da293d8e4298c08393e1283"));
@@ -28,7 +30,7 @@ public class SequentialGitAnalysisTest {
 
     @Test
     public void getParentCommits() throws Exception {
-        List<String> parentCommits = analysis.getParentCommits();
+        List<String> parentCommits = smallRepoAnalysis.getParentCommits();
         assertTrue(parentCommits.size() == 2);
         assertTrue(parentCommits.get(0).equals("34db5b16e46ec74f9da293d8e4298c08393e1283"));
         assertTrue(parentCommits.get(1).equals("504cd6a9abaaa0a7fb527570fb743fd315465e15"));
@@ -36,7 +38,7 @@ public class SequentialGitAnalysisTest {
 
     @Test
     public void getNumOfLines() throws Exception {
-        List<Integer> numOfLines = analysis.getNumOfLines();
+        List<Integer> numOfLines = smallRepoAnalysis.getNumOfLines();
         assertTrue(numOfLines.size() == 2);
         assertTrue(numOfLines.get(0) == 3);
         assertTrue(numOfLines.get(1) == 9);
